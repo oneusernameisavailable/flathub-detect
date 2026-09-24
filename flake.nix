3633a6dd5b60847586d8d234bdb4f0044c6d4863
Bazaar Developer <developer@example.com>
2026-09-24 23:47:58 +0200

    Add NixOS VM test via KVM
    - Add flake.nix with NixOS VM configuration for testing custom-installation
    - Add nixos-test-vm.nix with custom installation setup
    - Add .github/workflows/nixos-vm-test.yml for KVM-based VM testing
    - Tests custom-installation scenario on real NixOS VM with flatpak --installation=extra

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "NixOS VM for testing flathub-detect with custom installation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+    in {
+      # NixOS VM configuration
+      nixosConfigurations.flathub-test = pkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./nixos-test-vm.nix ];
+      };
+
+      # VM builder
+      nixosConfigurations.flathub-test-vm = pkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          { config, pkgs, ... }: {
+            imports = [ <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix> ];
+            
+            # Use the test VM config
+            imports = [ ./nixos-test-vm.nix ];
+            
+            # VM-specific settings
+            virtualisation = {
+              qemu = {
+                enable = true;
+                guestAgent.enable = true;
+                options = [
+                  "-m 1024"
+                  "-smp 2"
+                  "-nographic"
+                  "-serial stdio"
+                ];
+              };
+            };
+            
+            # Build VM image
+            system.build.vm = pkgs.runCommandLocal "nixos-vm" {
+              buildInputs = [ pkgs.qemu ];
+            } ''
+              mkdir -p $out
+              ${config.system.build.toplevel}/bin/run-nixos-vm -m 1024 -c 2 -snapshot -nographic > $out/run-vm.sh
+              chmod +x $out/run-vm.sh
+            '';
+          }
+        ];
+      };
+    in {
+      # VM runner script
+      defaultPackage.${system} = self.nixosConfigurations.flathub-test-vm.config.system.build.vm;
+      
+      # Also provide the configuration for reference
+      nixosConfigurations = {
+        flathub-test = self.nixosConfigurations.flathub-test;
+        flathub-test-vm = self.nixosConfigurations.flathub-test-vm;
+      };
+    }
+  );
+}
---
+++ nixos-test-vm.nix
@@ -0,0 +1,93 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # NixOS VM configuration for testing flathub-detect with custom installation
+  imports = [ <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix> ];
+
+  # System
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = false;
+  boot.kernelParams = [ "console=ttyS0" ];
+
+  # Network
+  networking = {
+    hostName = "flathub-test";
+    firewall.enable = false;
+  };
+
+  # SSH for testing
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      PasswordAuthentication = "yes";
+    };
+  };
+
+  # Root user with password
+  users.users.root = {
+    password = "test";
+    openssh.authorizedKeys.keys = [];
+  };
+
+  # Flatpak with custom installation
+  services.flatpak = {
+    enable = true;
+    extraInstallations = {
+      extra = {
+        path = "/var/lib/flatpak/extra";
+      };
+    };
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    flatpak
+    bash
+    curl
+    gnugrep
+    gnused
+  ];
+
+  # Add flathub remote to custom installation on first boot
+  systemd.services.flathub-setup = {
+    description = "Setup Flathub remote in custom installation";
+    after = [ "flatpak.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "/bin/bash -c 'flatpak --installation=extra remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo'";
+    };
+  };
+
+  # Auto-login for root on serial console
+  systemd.services.getty@ttyS0 = {
+    override = {
+      Service = {
+        ExecStart = "/sbin/agetty --autologin root --noclear %I $TERM";
+      };
+    };
+  };
+
+  # QEMU VM settings
+  virtualisation = {
+    qemu = {
+      enable = true;
+      guestAgent.enable = true;
+      options = [
+        "-m 1024"
+        "-smp 2"
+        "-nographic"
+        "-serial stdio"
+        "-drive file=disk.img,format=qcow2,if=virtio"
+      ];
+    };
+  };
+
+  # VM image
+  virtualisation.vmVariant = "qemu";
+  virtualisation.memorySize = 1024;
+  virtualisation.cores = 2;
+  virtualisation.diskSize = 8192;
+}
