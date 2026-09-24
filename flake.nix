766c315795f0d29643f1deeaf76a8c7f2d59898b
Bazaar Developer <developer@example.com>
2026-09-25 00:05:40 +0200

    Simplify flake.nix: remove flake-utils, use simpler structure to avoid syntax issues

--- flake.nix
+++ flake.nix
@@ -3,30 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-    in {
+      pkgs = import nixpkgs { config = { allowUnfree = true; }; };
+    in
+    {
       # NixOS VM configuration
-      nixosConfigurations.flathub-test = pkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./nixos-test-vm.nix ];
-      };
-
-      # VM builder
-      nixosConfigurations.flathub-test-vm = pkgs.lib.nixosSystem {
+      nixosConfigurations.flathub-test-vm = {
         system = "x86_64-linux";
         modules = [
-          { config, pkgs, ... }: {
+          {
             imports = [
               <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
               ./nixos-test-vm.nix
             ];
-            
-            # VM-specific settings
+
             virtualisation = {
               qemu = {
                 enable = true;
@@ -39,8 +32,7 @@
                 ];
               };
             };
-            
-            # Build VM image
+
             system.build.vm = pkgs.runCommandLocal "nixos-vm" {
               buildInputs = [ pkgs.qemu ];
             } ''
@@ -51,15 +43,5 @@
           }
         ];
       };
-    in {
-      # VM runner script
-      defaultPackage.${system} = self.nixosConfigurations.flathub-test-vm.config.system.build.vm;
-      
-      # Also provide the configuration for reference
-      nixosConfigurations = {
-        flathub-test = self.nixosConfigurations.flathub-test;
-        flathub-test-vm = self.nixosConfigurations.flathub-test-vm;
-      };
-    }
-  );
+    };
 }
