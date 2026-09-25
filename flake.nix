65b4ce35b5d85c0d5e34592f8504b22d288ab773
Bazaar Developer <developer@example.com>
2026-09-25 23:08:38 +0200

    fix: NixOS VM - use nixos-unstable for flatpak.extraInstallations support

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS VM for testing flathub-detect with custom installation";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
