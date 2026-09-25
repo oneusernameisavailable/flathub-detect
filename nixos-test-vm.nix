776fdc6dabf60ca9091156ea7411b0c7535bd691
Bazaar Developer <developer@example.com>
2026-09-26 00:19:20 +0200

    fix: NixOS VM - revert to working package output with qemu-vm import + inline system.build.vm

--- nixos-test-vm.nix
+++ nixos-test-vm.nix
@@ -1,118 +1,70 @@
-{ config, lib, pkgs, options, ... }:
+{ config, pkgs, lib, ... }:
 {
-  # Declare virtualisation options (normally provided by qemu-vm module)
-  options.virtualisation = {
-    memorySize = lib.mkOption {
-      type = lib.types.ints.positive;
-      default = 1024;
-      description = "Memory size in megabytes";
-    };
-    cores = lib.mkOption {
-      type = lib.types.ints.positive;
-      default = 1;
-      description = "Number of CPU cores";
-    };
-    diskSize = lib.mkOption {
-      type = lib.types.either (lib.types.enum [ "auto" ]) lib.types.ints.positive;
-      default = 1024;
-      description = "Disk size in megabytes";
-    };
-    vmVariant = lib.mkOption {
-      type = lib.types.attrs;
-      default = { };
-      description = "VM variant configuration";
-    };
-    qemu = {
-      options = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ ];
-        description = "QEMU options";
-      };
+  # NixOS VM configuration for testing flathub-detect with custom installation
+  # qemu-vm module imported via flake.nix
+
+  # System
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = false;
+  boot.kernelParams = [ "console=ttyS0" ];
+
+  # Network
+  networking = {
+    hostName = "flathub-test";
+    firewall.enable = false;
+  };
+
+  # SSH for testing
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      PasswordAuthentication = true;
     };
   };
 
-  config = {
-    # System
-    boot.loader.grub.enable = false;
-    boot.loader.systemd-boot.enable = false;
-    boot.kernelParams = [ "console=ttyS0" ];
-
-    # Network
-    networking = {
-      hostName = "flathub-test";
-      firewall.enable = false;
-    };
-
-    # SSH for testing
-    services.openssh = {
-      enable = true;
-      settings = {
-        PermitRootLogin = "yes";
-        PasswordAuthentication = true;
-      };
-    };
-
-    # Root user with password
-    users.users.root = {
-      password = "test";
-      openssh.authorizedKeys.keys = [];
-    };
-
-    # Flatpak with custom installation
-    services.flatpak.enable = true;
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-
-    # System packages
-    environment.systemPackages = with pkgs; [
-      flatpak
-      bash
-      curl
-      gnugrep
-      gnused
-    ];
-
-    # Add flathub remote to custom installation on first boot
-    systemd.services.flathub-setup = {
-      description = "Setup Flathub remote in custom installation";
-      after = [ "flatpak.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "/bin/bash -c 'mkdir -p /var/lib/flatpak/extra && flatpak --installation=extra remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo'";
-      };
-    };
-
-    # Auto-login for root on serial console
-    systemd.services."getty@ttyS0" = {
-      serviceConfig = {
-        ExecStart = "/sbin/agetty --autologin root --noclear %I $TERM";
-      };
-    };
-
-    # VM image settings
-    virtualisation.vmVariant = "qemu";
-    virtualisation.memorySize = 1024;
-    virtualisation.cores = 2;
-    virtualisation.diskSize = 8192;
-    virtualisation.qemu.options = [
-      "-m 1024"
-      "-smp 2"
-      "-nographic"
-      "-serial stdio"
-    ];
-
-    # Custom VM build output with run-vm.sh in root
-    system.build.vm = { config, pkgs, ... }: let
-      toplevel = config.system.build.toplevel;
-    in pkgs.runCommandLocal "nixos-vm" {
-      buildInputs = [ pkgs.qemu ];
-      toplevel = toplevel;
-    } ''
-      mkdir -p $out
-      ${toplevel}/bin/run-nixos-vm -m 1024 -c 2 -snapshot -nographic > $out/run-vm.sh
-      chmod +x $out/run-vm.sh
-    '';
+  # Root user with password
+  users.users.root = {
+    password = "test";
+    openssh.authorizedKeys.keys = [];
   };
+
+  # Flatpak with custom installation
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    flatpak
+    bash
+    curl
+    gnugrep
+    gnused
+  ];
+
+  # Add flathub remote to custom installation on first boot
+  systemd.services.flathub-setup = {
+    description = "Setup Flathub remote in custom installation";
+    after = [ "flatpak.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "/bin/bash -c 'mkdir -p /var/lib/flatpak/extra && flatpak --installation=extra remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo'";
+    };
+  };
+
+  # Auto-login for root on serial console
+  systemd.services."getty@ttyS0" = {
+    serviceConfig = {
+      ExecStart = "/sbin/agetty --autologin root --noclear %I $TERM";
+    };
+  };
+
+  # VM image (qemu-vm module options)
+  virtualisation.vmVariant = "qemu";
+  virtualisation.memorySize = 1024;
+  virtualisation.cores = 2;
+  virtualisation.diskSize = 8192;
 }
