6b4702f15c4188e24a6e81a5f38af7a3b5a5e65e
Bazaar Developer <developer@example.com>
2026-09-26 00:15:34 +0200

    fix: NixOS VM - wrap config in explicit config attribute for module with options

--- nixos-test-vm.nix
+++ nixos-test-vm.nix
@@ -31,74 +31,76 @@
     };
   };
 
-  # System
-  boot.loader.grub.enable = false;
-  boot.loader.systemd-boot.enable = false;
-  boot.kernelParams = [ "console=ttyS0" ];
+  config = {
+    # System
+    boot.loader.grub.enable = false;
+    boot.loader.systemd-boot.enable = false;
+    boot.kernelParams = [ "console=ttyS0" ];
 
-  # Network
-  networking = {
-    hostName = "flathub-test";
-    firewall.enable = false;
-  };
-
-  # SSH for testing
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "yes";
-      PasswordAuthentication = true;
+    # Network
+    networking = {
+      hostName = "flathub-test";
+      firewall.enable = false;
     };
-  };
 
-  # Root user with password
-  users.users.root = {
-    password = "test";
-    openssh.authorizedKeys.keys = [];
-  };
-
-  # Flatpak with custom installation
-  services.flatpak.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-
-  # System packages
-  environment.systemPackages = with pkgs; [
-    flatpak
-    bash
-    curl
-    gnugrep
-    gnused
-  ];
-
-  # Add flathub remote to custom installation on first boot
-  systemd.services.flathub-setup = {
-    description = "Setup Flathub remote in custom installation";
-    after = [ "flatpak.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = "/bin/bash -c 'mkdir -p /var/lib/flatpak/extra && flatpak --installation=extra remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo'";
+    # SSH for testing
+    services.openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "yes";
+        PasswordAuthentication = true;
+      };
     };
-  };
 
-  # Auto-login for root on serial console
-  systemd.services."getty@ttyS0" = {
-    serviceConfig = {
-      ExecStart = "/sbin/agetty --autologin root --noclear %I $TERM";
+    # Root user with password
+    users.users.root = {
+      password = "test";
+      openssh.authorizedKeys.keys = [];
     };
-  };
 
-  # VM image settings
-  virtualisation.vmVariant = "qemu";
-  virtualisation.memorySize = 1024;
-  virtualisation.cores = 2;
-  virtualisation.diskSize = 8192;
-  virtualisation.qemu.options = [
-    "-m 1024"
-    "-smp 2"
-    "-nographic"
-    "-serial stdio"
-  ];
+    # Flatpak with custom installation
+    services.flatpak.enable = true;
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+
+    # System packages
+    environment.systemPackages = with pkgs; [
+      flatpak
+      bash
+      curl
+      gnugrep
+      gnused
+    ];
+
+    # Add flathub remote to custom installation on first boot
+    systemd.services.flathub-setup = {
+      description = "Setup Flathub remote in custom installation";
+      after = [ "flatpak.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "/bin/bash -c 'mkdir -p /var/lib/flatpak/extra && flatpak --installation=extra remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo'";
+      };
+    };
+
+    # Auto-login for root on serial console
+    systemd.services."getty@ttyS0" = {
+      serviceConfig = {
+        ExecStart = "/sbin/agetty --autologin root --noclear %I $TERM";
+      };
+    };
+
+    # VM image settings
+    virtualisation.vmVariant = "qemu";
+    virtualisation.memorySize = 1024;
+    virtualisation.cores = 2;
+    virtualisation.diskSize = 8192;
+    virtualisation.qemu.options = [
+      "-m 1024"
+      "-smp 2"
+      "-nographic"
+      "-serial stdio"
+    ];
+  };
 }
