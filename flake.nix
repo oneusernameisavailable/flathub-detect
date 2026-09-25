e8b64f69a5c13ae74fea04eff3e70f2d0f0fc7a4
Bazaar Developer <developer@example.com>
2026-09-25 17:41:06 +0200

    Fix flake.nix: define VM as package first, then nixosConfigurations

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
       pkgs = import nixpkgs { config = { allowUnfree = true; }; };
     in
     {
-      nixosConfigurations.flathub-test-vm = {
+      # First define the VM as a package directly
+      packages.x86_64-linux.flathub-test-vm = pkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           {
@@ -46,6 +47,40 @@
         ];
       };
 
-      packages.x86_64-linux.flathub-test-vm = self.nixosConfigurations.flathub-test-vm.config.system.build.vm;
+      nixosConfigurations.flathub-test-vm = pkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          {
+            imports = [
+              <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
+              ./nixos-test-vm.nix
+            ];
+
+            virtualisation = {
+              qemu = {
+                enable = true;
+                guestAgent.enable = true;
+                options = [
+                  "-m 1024"
+                  "-smp 2"
+                  "-nographic"
+                  "-serial stdio"
+                ];
+              };
+            };
+
+            system.build.vm = { config, pkgs, ... }: let
+              toplevel = config.system.build.toplevel;
+            in pkgs.runCommandLocal "nixos-vm" {
+              buildInputs = [ pkgs.qemu ];
+              toplevel = toplevel;
+            } ''
+              mkdir -p $out
+              ${toplevel}/bin/run-nixos-vm -m 1024 -c 2 -snapshot -nographic > $out/run-vm.sh
+              chmod +x $out/run-vm.sh
+            '';
+          }
+        ];
+      };
     };
 }
