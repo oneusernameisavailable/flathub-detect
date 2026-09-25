2be5a4c48e18650449742b607a7ea3fb8875a51e
Bazaar Developer <developer@example.com>
2026-09-26 00:23:23 +0200

    fix: NixOS VM - remove semicolon after heredoc in mkForce function

--- flake.nix
+++ flake.nix
@@ -25,8 +25,8 @@
               mkdir -p $out
               ${toplevel}/bin/run-nixos-vm -m 1024 -c 2 -snapshot -nographic > $out/run-vm.sh
               chmod +x $out/run-vm.sh
-            '';
-            });
+            ''
+            );
           }
         ];
       }).config.system.build.vm;
