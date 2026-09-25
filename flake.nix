16fb9181b3519079cbb129bdff23ac78fc7ca797
Bazaar Developer <developer@example.com>
2026-09-25 17:57:36 +0200

    Fix flake.nix: simplify to use nixpkgs.lib.nixosSystem directly

--- flake.nix
+++ flake.nix
@@ -42,44 +42,8 @@
         }
       ];
 
-      vmPackage = pkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          {
-            imports = [
-              <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
-              ./nixos-test-vm.nix
-            ];
-
-            virtualisation = {
-              qemu = {
-                enable = true;
-                guestAgent.enable = true;
-                options = [
-                  "-m 1024"
-                  "-smp 2"
-                  "-nographic"
-                  "-serial stdio"
-                ];
-              };
-            };
-
-            system.build.vm = { config, pkgs, ... }: let
-              toplevel = config.system.build.toplevel;
-            in pkgs.runCommandLocal "nixos-vm" {
-              buildInputs = [ pkgs.qemu ];
-              toplevel = toplevel;
-            } ''
-              mkdir -p $out
-              ${toplevel}/bin/run-nixos-vm -m 1024 -c 2 -snapshot -nographic > $out/run-vm.sh
-              chmod +x $out/run-vm.sh
-            '';
-          }
-        ];
-      };
-
     in {
-      packages.x86_64-linux.flathub-test-vm = pkgs.lib.nixosSystem {
+      packages.x86_64-linux.flathub-test-vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           {
